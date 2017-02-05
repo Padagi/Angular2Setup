37a5aa49d0fda16962082da4b0dd958bf798fedc
Matthew Wood <matthewjameswood@outlook.com>
2017-02-04 19:50:46 -0800

    Updated TS file.

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -4,4 +4,4 @@
     selector: 'my-app',
     template: '<h1>Hello {{name}}</h1>',
 })
-export class AppComponent { name = 'Angular Testing 004'; }
+export class AppComponent { name = 'Angular Testing 100'; }
